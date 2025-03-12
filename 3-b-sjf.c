81fe373fbe424aed16ad1c2f0ca6ec1f03927333
CS23students <153345000+CS23students@users.noreply.github.com>
2025-03-12 20:06:51 +0530

    Create 3-b-sjf.c

---
+++ 3-b-sjf.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    int bt[20], p[20], wt[20], tat[20];
+    float avg_wt = 0, avg_tat = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter burst time for each process:\n");
+    for (i = 0; i < n; i++) {
+        printf("P%d: ", i + 1);
+        scanf("%d", &bt[i]);
+        p[i] = i + 1;
+    }
+
+    // Sorting processes by burst time
+    for (i = 0; i < n; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (bt[i] > bt[j]) {
+                temp = bt[i]; bt[i] = bt[j]; bt[j] = temp;
+                temp = p[i]; p[i] = p[j]; p[j] = temp;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    for (i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
+    }
+
+    for (i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        avg_wt += wt[i];
+        avg_tat += tat[i];
+    }
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
+    }
+
+    printf("\nAverage Waiting Time = %.2f", avg_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", avg_tat / n);
+
+    return 0;
+}
+
+
+// OP :
+// Enter number of processes: 4
+// Enter burst time for each process:
+// P1: 6
+// P2: 8
+// P3: 7
+// P4: 3
+// Process  Burst Time  Waiting Time  Turnaround Time
+// P4       3           0             3
+// P1       6           3             9
+// P3       7           9             16
+// P2       8           16            24
+
+// Average Waiting Time = 7.00
+// Average Turnaround Time = 13.00
