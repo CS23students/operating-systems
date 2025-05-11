2ba3b96f2f39c23f0f65d9930105ffe61f49a258
CS23students <153345000+CS23students@users.noreply.github.com>
2025-05-11 15:16:01 +0530

    Create 5-display-sys-info.sh

---
+++ 1-b-shell-prgs/5-display-sys-info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "System Information:"
+echo "--------------------"
+echo "User: $(whoami)"
+echo "OS: $(uname -s)"
+echo "Kernel Version: $(uname -r)"
+echo "Current Directory: $(pwd)"
+echo "System Uptime: $(uptime -p)"
+
+
+# System Information:
+# --------------------
+# User: CS23students
+# OS: MINGW64_NT-10.0
+# Kernel Version: 3.1.0-340
+# Current Directory: /c/Users/cs23students
+# System Uptime: up 5 hours, 30 minutes
