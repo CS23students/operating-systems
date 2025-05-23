3a77cd86386dd312b6303f77f9f11d858ad1a70d
CS23students <153345000+CS23students@users.noreply.github.com>
2025-05-23 19:59:52 +0530

    Create exit.c

---
+++ 2-system-calls/exit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Program started\n");
+
+    exit(0); // Terminates the process
+
+    printf("This will not be printed\n");
+    return 0;
+}
